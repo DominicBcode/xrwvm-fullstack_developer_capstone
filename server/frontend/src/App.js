5824fcc8890fb659fe71c1e33cb6efd0b0ec62d7
DominicBcode <dominiceighth@gmail.com>
2024-11-04 21:41:36 -0500

    changed

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,20 @@
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
+import SearchCars from "./components/Dealers/SearchCars"
+import PostReview from "./components/Dealers/PostReview"
 import LoginPanel from "./components/Login/Login"
-import Register from "./components/Register/Register";
+import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/searchcars/:id" element={<SearchCars/>} />
     </Routes>
   );
 }
